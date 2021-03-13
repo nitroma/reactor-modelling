78ed2213a56519bdc8e7739a8cec63ce8341ecde
Andreas Richardson <atr2717@ic.ac.uk>
2021-03-13 23:12:37 +0000

    switch to gamultiobj

--- comsol_optimisation.m
+++ comsol_optimisation.m
@@ -1,29 +1,34 @@
 %% Multivariate optimisation using a COMSOL simulation
+clear, clc, diary on
 
 % initial parameter values
 pname = ["d_i" "m_c_in" "Tc_in" "Tc"];
 punit = ["[m]" "[kg/s]" "[K]"   "[K]"];
-x0    = [0.02; 0.15;    325;    330];
+x0    = [0.02  0.15     325     330];
+lb    = [5e-3  0.05     273     273];
+ub    = [5e-2  0.50     373     373];
 
 % create function wrapper
 wrapped_comsol = @(x) do_comsol(x,pname,punit);
 
-% set up for optimisation
-goal = [363, 3]; % T, X_TOL<0.98
-weight = goal;
+% don't bother with the inequality or equality constrains
+A = zeros(1,4); b = 0;
+Aeq = A; beq = b;
 
 % do optimisation
-x = fgoalattain(wrapped_comsol,x0,goal,weight);
+x = gamultiobj(wrapped_comsol,4,A,b,Aeq,beq,lb,ub);
 writematrix(x,'comsol_optimised_values.txt');
+
+diary off
 
 function F = do_comsol(x,pname,punit)
 % key reference: https://uk.comsol.com/blogs/how-to-run-comsol-multiphysics-from-the-command-line/
 
 % read input
-pvals = string(num2str(x,'%1.3e'));
+pvals = string(num2str(x(:),'%1.3e'))';
 
 % generate command
-command = strcat("comsolbatch -inputfile in.mph -pname ",join(pname,',')," -plist ",join(strcat('"',strcat(pvals,punit'),'"'),',')," -methodcall my_method -nosave");
+command = strcat("comsolbatch -inputfile in.mph -pname ",join(pname,',')," -plist ",join(strcat('"',join([pvals;punit],'',1),'"'),',')," -methodcall my_method -nosave");
 disp(command)
 
 % execute command
